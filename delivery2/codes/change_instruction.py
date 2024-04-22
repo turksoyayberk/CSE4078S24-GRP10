22f9f926816fcb7c4d3e51238afca5a8204b44ca
Ayberk <ayberkturksoy97@gmail.com>
2024-04-22 16:52:38 +0300

    Update change_instruction.py

--- delivery2/codes/change_instruction.py
+++ delivery2/codes/change_instruction.py
@@ -4,7 +4,7 @@ with open('all_sağlık.json', 'r', encoding='utf-8') as file:
     data = json.load(file)
 
 for item in data:
-    item['instruction'] = "Aşağıdaki cümlenin Dünya, Ekonomi, Kültür, Sağlık, Spor, Teknoloji, Ürün Yorumu, Dizi-Film, Şikayet-Müşteri Geri Bildirim, Magazin kategorilerinden hangisine ait olduğunu söyle."
+    item['instruction'] = "Aşağıdaki cümlenin Dünya, Ekonomi, Kültür, Sağlık, Spor, Teknoloji, Ürün Yorumu, Dizi-Film, Şikayet-Müşteri Geri Bildirim, Magazin, Kitap kategorilerinden hangisine ait olduğunu söyle."
 
 with open('all_sağlık2.json', 'w', encoding='utf-8') as file:
     json.dump(data, file, ensure_ascii=False, indent=4)
